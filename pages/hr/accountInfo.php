9b8e6308053bf47fe1e8dff4ed04d4c1a9fad82f
VelizarStavrev <velizar.g.stavrev@gmail.com>
2021-02-23 22:43:43 +0200

    refactoring - adding an auth folder

--- pages/hr/accountInfo.php
+++ pages/hr/accountInfo.php
@@ -8,23 +8,6 @@
     
 ?>
 
-<?php 
-
-    if (!$_SESSION['logged_in'] || $_SESSION['role_id'] != 2) {
-
-        if (!$_SESSION['role_id']) {
-            $_SESSION['logged_in'] = null;
-        }
-
-        $host  = $_SERVER['HTTP_HOST'];
-        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = '../login.php';
-        header("Location: http://$host$uri/$extra");
-        exit;
-        
-    } else {
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -205,9 +188,3 @@
 </body>
 
 </html>
-
-<?php
-
-    }
-
-?>
